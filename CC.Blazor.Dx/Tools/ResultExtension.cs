13ffc5ef3f3149bbee50347a6547ec9a2b82b92a
TimChen <timchen@live.com>
2022-01-17 09:37:49 +0800

    提交

---
+++ CC.Blazor.Dx/Tools/ResultExtension.cs
@@ -0,0 +1,27 @@
+﻿using CC.Blazor.Dx;
+using CC.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCP.Blazor
+{
+    public static class ResultExtension
+    {
+        /// <summary>
+        /// 如果结果失败就弹出提示
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="boxSrv"></param>
+        /// <returns></returns>
+        public static bool FailAndPrompt(this Result result, CcToastService toastSrv)
+        {
+            if (result.IsOK == true) return false;
+            toastSrv.Error(result.Message ?? "返回失败");
+            return true;
+        }
+
+    }
+}
